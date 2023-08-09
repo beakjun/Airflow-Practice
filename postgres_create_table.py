c244278b100cab7616e5570459d32d414010620d
beakjun <wjsqorwns93@naver.com>
2023-08-09 18:02:24 +0900

    postgres_create_table test

---
+++ postgres_create_table.py
@@ -0,0 +1,36 @@
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow import DAG
+import airflow
+
+dag=DAG(
+    dag_id='create_table',
+    start_date=airflow.utils.dates.days_ago(1),
+    schedule_interval=None
+)
+create_employees_table = PostgresOperator(
+    task_id="create_employees_table",
+    postgres_conn_id="sr-postgres",
+    sql="""
+        CREATE TABLE IF NOT EXISTS employees (
+            "Serial Number" NUMERIC PRIMARY KEY,
+            "Company Name" TEXT,
+            "Employee Markme" TEXT,
+            "Description" TEXT,
+            "Leave" INTEGER
+        );""",
+)
+
+create_employees_temp_table = PostgresOperator(
+    task_id="create_employees_temp_table",
+    postgres_conn_id="sr-postgres",
+    sql="""
+        DROP TABLE IF EXISTS employees_temp;
+        CREATE TABLE employees_temp (
+            "Serial Number" NUMERIC PRIMARY KEY,
+            "Company Name" TEXT,
+            "Employee Markme" TEXT,
+            "Description" TEXT,
+            "Leave" INTEGER
+        );""",
+    dag=dag
+)
