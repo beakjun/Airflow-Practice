9c85bd0d4f0a4bc1d37847fd7eee6a29454b26b6
beakjun <wjsqorwns93@naver.com>
2023-08-23 15:42:36 +0900

    REST API stock market data complete

--- stock_market_crawling_1.py
+++ stock_market_crawling_1.py
@@ -2,12 +2,20 @@ import pendulum
 from airflow.decorators import dag, task
 import pandas as pd
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.operators.python import PythonOperator
-import logging
-import html5lib
+import requests
+from sqlalchemy import create_engine
 # [END import_module]
 
 
+
+
+#param={'Servicekey': key ,'resultType':'json','numOfRows':3000,'basDt':'20230821'}
+url='https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?'
+# response=requests.get(url,param)
+
+
+
+# df_contacts = pd.json_normalize(datajs, record_path=['response','body','items','item'])
 
 def get_postgres(autocommit=True):
     hook = PostgresHook(postgres_conn_id = 'sr-postgres')
@@ -15,35 +23,15 @@ def get_postgres(autocommit=True):
     conn.autocommit = autocommit
     return conn.cursor()
 
-jmk_list = ['005930','011200']
 
-
-
-@task
-def url_gen(url,code):
-    new_url = f'{str(url)}code={code}'
-    print(new_url)
-    return new_url
-
-@task
-def html_request(url,edate):
-    date=edate.strftime('%Y.%m.%d')
-    page=0
-    while True:
-        if page>2000:
-            break
-        page+=1
-        new_url=f'{url}&page={str(page)}'
-        df=pd.read_html(new_url)[0]
-        if date in list(df['날짜']):
-            return df[df['날짜']==date]
     
+
 
 # [START instantiate_dag]
 @dag(
     dag_id = 'stock_market_crawling_1',
-    schedule_interval='30 16 * * 1-5',
-    start_date=pendulum.datetime(2023, 7, 25, 16 ,30 , tz='Asia/Seoul'),
+    schedule_interval='0 12 * * 1-5',
+    start_date=pendulum.datetime(2023, 7, 1, 16 ,30 , tz='Asia/Seoul'),
     #end_date = pendulum.now(),
     catchup=True,  # backfill과 비슷한 기능 
     tags=['crawling'],
@@ -56,17 +44,36 @@ def stock_market_crawling():
 
     @task
     def date_execution(**kwargs):
-        execution_date=kwargs['data_interval_end']
-        #execution_date=local_tz.convert(execution_date)
-        logging.info(f'collection date : {execution_date}')
+        execution_date=kwargs['logical_date']
         return execution_date
     
-    url = 'https://finance.naver.com/item/sise_day.naver?'
-    edate=date_execution()
-    for item in jmk_list:
-        html_request.override(task_id=f'html_request_{item}')(url_gen.override(task_id=f'url_gen_{item}')(url,item),edate)
+
+    @task
+    def html_request(url,bsdt):
+        key='VtJk4y5W9b0T3ZOFuwD8v+nyzxuOEfRsSeIU8pnok9bPZpxQ40a9qzwoMB38tnJgog/lvxMAxNBJNMpt4f482A=='
+        bsdt=bsdt.strftime('%Y%m%d')
+        param = {'Servicekey': key ,'resultType':'json','numOfRows':3000,'basDt':bsdt}
+        response=requests.get(url,param)
+        print(response.text)
+        if response.status_code != 200:
+            raise ValueError(f"Request failed with status code {response.status_code}")
+        data=response.json()
+        if len(date)==0:
+            raise ValueError("Empty Data")
+        df = pd.json_normalize(data,record_path=['response','body','items','item'])
+        return df
+    
+    @task
+    def load(table_nm,df):
+        postgres_hook = PostgresHook('sr-postgres')
+        conn=postgres_hook.get_conn()
+        engine=create_engine(postgres_hook.get_uri(), echo=False)
+        df.to_sql(table_nm,engine,schema='test',if_exists='append',index=False)
 
 # [START dag_invocation]
+    bsdt=date_execution()
+    df=html_request(url,bsdt)
+    load('stock_market_tbl',df)
 stock_market_crawling()
 # [END dag_invocation]
 
